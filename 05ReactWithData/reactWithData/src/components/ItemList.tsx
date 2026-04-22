bf6145848f5b8eab1ff540a9fc5f056a833780f9
Opilane TTHK <opilane@tthk.ee>
2026-04-22 13:53:52 +0300

    components

--- 05ReactWithData/reactWithData/src/components/ItemList.tsx
+++ 05ReactWithData/reactWithData/src/components/ItemList.tsx
@@ -5,5 +5,14 @@
 
 
 export default function ItemList({items, onDelete}: any) {
-    
+    return (
+        <ul>
+            {items.map((i: any) => (
+                <li key={i.id}>
+                    {i.name}
+                    <button onClick = {() => onDelete(i.id)}>X</button>      
+                </li>
+            ))}
+        </ul>
+    );
 }
